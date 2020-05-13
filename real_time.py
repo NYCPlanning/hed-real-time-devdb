a1c1b7c0474b98ad0e53a1a9db6e8f5c573940b5
Te Du <tedudevelop@gmail.com>
2020-05-13 18:37:35 -0400

    embed url in text

--- real_time.py
+++ real_time.py
@@ -221,9 +221,9 @@ def main():
     st.info("""
     
     On March 27, 2020 NYS issued a ban on all nonessential construction in 
-    effort to contain Covid-19. Since this time, the definition of [**essential**](www.google.com) has widened 
+    effort to contain Covid-19. Since this time, the definition of [**essential**](https://www1.nyc.gov/assets/buildings/pdf/essential_vs_non-essential.pdf) has widened 
     somewhat but reductions in building application and permit activity are still apparent. 
-    This tracker displays weekly data [**updates**](www.google.com) from the Department of Buildings, showing major 
+    This tracker displays weekly data [**updates**](https://data.cityofnewyork.us/Housing-Development/DOB-Job-Application-Filings/ic3t-wcy2) from the Department of Buildings, showing major 
     construction (new buildings, major alterations, and demolitions) at three important 
     milestones (application filed, permit issued, and certificate of occupancy issued).  
       
@@ -248,7 +248,7 @@ def main():
     + Despite the relatively small number of applications for new residential buildings, the number of units associated with those jobs is still high, suggesting that applicants are primarily submitting for larger jobs. 
     
     ### Permits Issued
-    + Covid-19 and the construction ban had the greatest impact on permits issued, since permits are only currently being issued for essential construction.  
+    + Covid-19 and the construction ban had the greatest impact on permits issued, since permits are only currently being issued for [**essential construction**](https://www1.nyc.gov/assets/buildings/html/essential-active-construction.html).  
     + 2020 permits for all new buildings, major alterations, and demolitions were keeping pace with the last three years until week 12 (March 16), when they began to drop to only one or two per week. The few permits that have been issued since the construction ban took effect on March 27 (week 14) include residential and non-residential buildings alike. 
     
     ### Certificates of Occupancy
@@ -275,7 +275,7 @@ def fill_zeros(agg_db, conn):
         
         if year == 2020:
             
-            new = new.loc[new.week <= int(datetime.datetime.now().strftime("%V"))]
+            new = new.loc[new.week < int(datetime.datetime.now().strftime("%V"))]
 
         df = pd.concat([df, new], axis=0, sort=True)
 
